b101c959178aa53c6b3ad11a62e463fa7c695c35
Maheshwariakshita <89269759+Maheshwariakshita@users.noreply.github.com>
2024-07-11 13:07:45 +0530

    Time: 66 ms (44.80%) | Memory: 18.1 MB (7.33%) - LeetSync

---
+++ 1-two-sum/two-sum.py
@@ -0,0 +1,118 @@
+class Solution:
+
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+            h={}
+            for curr in nums:
+                if curr+h[target-curr]==target:
+                    return [curr,target-curr]
+                    
+                else:
+                    h[target-curr]=curr
+       
+       
+       
+       
+       
+       
+       
+        # i=0
+        # j=len(nums)-1
+        # while i<j:
+        #     if nums[i]+nums[j]==target:
+        #         return [i,j]
+        #     elif 
+
+
+
+
+
+
+        # #two pointer
+        # i=0
+        # j=len(nums)-1
+        # while i<j:
+        #     if nums[i]+nums[j]==target:
+        #         return [i, j]
+        #     i+=1
+        #     j-=1
+        # return
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# class Solution:
+
+#     def twoSum(self, nums: List[int], target: int) -> List[int]:
+#         h={}
+
+#         for i, j in enumerate(nums):
+#             diff=target-j
+#             if diff in h:
+#                 return [j, h[j]]
+#             else:
+#                 h[j]=i
+
+
+
+# class Solution:
+
+#     def twoSum(self, nums: List[int], target: int) -> List[int]:
+
+#         for i in range (len(a)):
+#             for j in range (i+1,len(a)):
+#                 if a[i]+a[j]==sum:
+#                     if(a[i]<a[j]):
+#                         print(a[i]," ",a[j])
+#                     else:
+#                         print(a[j]," ",a[i])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        nums = enumerate(nums)
+        nums = sorted(nums, key=lambda x:x[1])
+        l, r = 0, len(nums)-1
+        while l < r:
+            if nums[l][1]+nums[r][1] == target:
+                return sorted([nums[l][0], nums[r][0]])
+            elif nums[l][1]+nums[r][1] < target:
+                l += 1
+            else:
+                r -= 1
+        return 
