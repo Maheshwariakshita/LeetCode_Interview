1ca2e14674d930c98bd6582d21a0936308e0eca1
Maheshwariakshita <89269759+Maheshwariakshita@users.noreply.github.com>
2024-07-21 10:51:57 +0530

    Time: 70 ms (42.46%) | Memory: 18.1 MB (7.21%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,73 +1,35 @@
-class Solution:
-
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        for i in range(len(nums)):
-            for j in range(i+1, len(nums)):
-                if nums[i]+nums[j]==target:
-                    return [i, j]
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        # 1
-        # i=0
-        # j=len(nums)-1
-        # for i in range(len(nums)):
-        #     for j in range(i+1,len(nums)):
-        #         if nums[i]+nums[j]==target:
-        #             return [i,j]
-               
-       
-       
-       
-       
-        # 2
-        # h = {}
-
-        # for i, num in enumerate(nums):
-        #     diff = target - num
-        #     if diff in h:
-        #         return [h[diff], i]  # Correct order of indices
-        #     else:
-        #         h[num] = i  # Store the index as the value
-        # return []
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 # class Solution:
+
 #     def twoSum(self, nums: List[int], target: int) -> List[int]:
-#         nums = enumerate(nums)
-#         nums = sorted(nums, key=lambda x:x[1])
-#         l, r = 0, len(nums)-1
-#         while l < r:
-#             if nums[l][1]+nums[r][1] == target:
-#                 return sorted([nums[l][0], nums[r][0]])
-#             elif nums[l][1]+nums[r][1] < target:
-#                 l += 1
-#             else:
-#                 r -= 1
-#         return 
+#         # 2 hashmap
+#         h={}
+#         for i, curr in enumerate(nums):     # {0:2, 1:7, 2:11, 3:15}
+#             diff=target-curr
+#             if diff in h:
+#                 return [i, h[diff]]
+#             h[curr]=i
+
+#     # h={2:0, 7:1, ......} h[curr]=i
+
+
+        # 1 brute force solution
+        # for i in range(len(nums)):
+        #     for j in range(i+1, len(nums)):
+        #         if nums[i]+nums[j]==target:
+        #             return [i, j]
+   
+       
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        nums = enumerate(nums)
+        nums = sorted(nums, key=lambda x:x[1])
+        l, r = 0, len(nums)-1
+        while l < r:
+            if nums[l][1]+nums[r][1] == target:
+                return sorted([nums[l][0], nums[r][0]])
+            elif nums[l][1]+nums[r][1] < target:
+                l += 1
+            else:
+                r -= 1
+        return 
