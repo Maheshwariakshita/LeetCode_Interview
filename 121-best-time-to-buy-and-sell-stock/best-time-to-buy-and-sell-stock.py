a1cb65132c9a442c0038f13e203383d254e9b9f3
Maheshwariakshita <89269759+Maheshwariakshita@users.noreply.github.com>
2024-07-31 18:19:43 +0530

    Time: 801 ms (12.61%) | Memory: 27.5 MB (25.49%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -1,32 +1,12 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        if not prices:
-            return 0
-        
-        min_price = float('inf')
-        max_profit = 0
-        
-        for price in prices:
-            if price < min_price:
-                min_price = price
-            elif price - min_price > max_profit:
-                max_profit = price - min_price
-        
-        return max_profit
+        # 1 diff maximum via loop
+        #2 pointer approach 0, len-1-> 
+        #3 max, min- diff 
 
-# class Solution:
-#     def maxProfit(self, prices: List[int]) -> int:
-#         mini=0
-#         maxi=mini
-#         for i in range(len(prices)): 
-#             for j in range(len(prices)):
-#                 if prices[i]<prices[j]:
-#                     mini=i #now we have index mini num
-        
-#         for k in range(mini+1, len(prices)):
-#             for l in range(mini+1, len(prices)):
-#                 if prices[k]>prices[l]:
-#                     maxi=k
-#         return prices[maxi]-prices[mini]
-        
-        
+        mini=prices[0]
+        ans=0
+        for i in range(len(prices)):
+            mini=min(mini, prices[i])
+            ans=max(ans, prices[i]-mini) 
+        return ans
