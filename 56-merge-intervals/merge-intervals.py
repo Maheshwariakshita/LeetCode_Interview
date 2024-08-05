efa3178bdcc2e0eb25f20c41dab480e51345711c
Maheshwariakshita <89269759+Maheshwariakshita@users.noreply.github.com>
2024-08-05 11:53:21 +0530

    Time: 126 ms (42.87%) | Memory: 20.5 MB (64.14%) - LeetSync

--- 56-merge-intervals/merge-intervals.py
+++ 56-merge-intervals/merge-intervals.py
@@ -1,19 +1,21 @@
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-        # intervals.sort() # or a=sorted(intervals)
-        # a=[]
+        intervals.sort(key=lambda x: x[0]) # or a=sorted(intervals)
+        a=[intervals[0]]
 
-        # if len(intervals)<2:
-        #     return intervals
-                
-        # for i in range(len(intervals)-1):
-        #     if intervals[i][1]>=intervals[i+1][0]:
-        #         a.append([intervals[i][0], intervals[i+1][1]])
-        #     else:
-        #         # a.append([intervals[i][0], intervals[i][1]])
-        #         # a.append(intervals[i])
-        #         a.append(intervals[i+1])
-        # return a
+        if len(intervals)<2:
+            return intervals
+         
+        for i in range(1,len(intervals)):
+            current=intervals[i]
+            previous=a[-1]
+            if previous[1]>=current[0]:
+                a[-1]=[previous[0], max(previous[1],current[1])]
+            else:
+                # a.append([intervals[i][0], intervals[i][1]])
+                # a.append(intervals[i])
+                a.append(current)
+        return a
 
 
 
@@ -26,23 +28,23 @@ class Solution:
         
 # class Solution:
 #     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-        if len(intervals) <= 1:
-            return intervals
+        # if len(intervals) <= 1:
+        #     return intervals
 
-        # Sort intervals based on the start value
-        intervals.sort(key=lambda x: x[0])
+        # # Sort intervals based on the start value
+        # intervals.sort(key=lambda x: x[0])
 
-        merged_intervals = [intervals[0]]
+        # merged_intervals = [intervals[0]]
 
-        for i in range(1, len(intervals)):
-            current_interval = intervals[i]
-            previous_interval = merged_intervals[-1]
+        # for i in range(1, len(intervals)):
+        #     current_interval = intervals[i]
+        #     previous_interval = merged_intervals[-1]
 
-            if previous_interval[1] >= current_interval[0]:
-                # Merge the overlapping intervals
-                merged_intervals[-1] = [previous_interval[0], max(previous_interval[1], current_interval[1])]
-            else:
-                # Add non-overlapping interval to the result
-                merged_intervals.append(current_interval)
+        #     if previous_interval[1] >= current_interval[0]:
+        #         # Merge the overlapping intervals
+        #         merged_intervals[-1] = [previous_interval[0], max(previous_interval[1], current_interval[1])]
+        #     else:
+        #         # Add non-overlapping interval to the result
+        #         merged_intervals.append(current_interval)
 
-        return merged_intervals
+        # return merged_intervals
